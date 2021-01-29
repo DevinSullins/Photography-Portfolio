fb41c426fbfaa2b3a872628c201363bc37fa5167
Devin Sullins <devinsullins@gmail.com>
2021-01-29 11:27:32 -0600

    created Projects, Articles, About pages, linked them to App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
+import { BrowserRouter, Route, Link } from 'react-router-dom'; 
+import Projects from './Projects.js'; 
+import Articles from './Articles.js'; 
+import About from './About.js'; 
 import './App.css';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      //set up router
+      <Route path="/" component={Projects}/>
+      <Route path="/articles" component={Articles}/>
+      <Route path="/about" component={About}/>
+
+      <div className='navigation'>
+        <img src={logo} className="logo" alt="Logo Image" />
+        <div className="navigation-sub">
+
+          //links to pages
+          <Link to="/" className="item">Projects</a>
+          <Link to="/articles" className="item">Articles</a>
+          <Link to="/about" className="item">About</a>
+        </div>
+      </div>
     </div>
+    </BrowserRouter>
   );
 }
 
